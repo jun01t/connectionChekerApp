d4be513dd864a6a3f9ac45e472dfb00d9f3d48d1
TaharaNoNeko <tmdjnch0901@gmail.com>
2021-02-19 10:46:00 +0900

    first commit

--- statusbar_test/ViewController.swift
+++ statusbar_test/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import SystemConfiguration
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func checkConnection(_ sender: Any) {
+        if(connectedToNetwork()){
+            label.text = "connected!"
+        }else{
+            label.text = "disconnected!"
+        }
+    }
+    
+    func connectedToNetwork() -> Bool {
 
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
 
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+
+        return (isReachable && !needsConnection)
+    }
 }
 
